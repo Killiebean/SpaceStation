55e69dee24684a61718fb6e40b86763279790955
Killiebean <aquielle.campbell@gmail.com>
2025-12-12 23:14:53 +0000

    save1

--- SpaceStation.py
+++ SpaceStation.py
@@ -1,6 +1,6 @@
-# Name: 
-# Date:
-# Period:
+# Name: Aquielle Campbell   
+# Date: 12/11/25
+# Period: 4
 # Lab: SpaceStation.py
 # Description: Students use web services to gather and process live data from the internet.
 #
@@ -12,22 +12,71 @@
 import json  # to interpret JSON data from a web service
 import urllib.request  # to load data from a specific web service api
 import turtle  # for images
+import time # for time.sleep
 
 # TODO: Step 1 Who is in space?
+url = 'http://api.open-notify.org/astros.json'
 
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+
+# print(result)
+print('there are ',result['number'],' people in space.')
+
+print('They are:')
+for astro in result['people']:
+    print('\t', astro['name'])
 
 # TODO: Step 2 Where is the ISS?
+time.sleep(1) # pause 1 sec before load map
+
+url = 'http://api.open-notify.org/iss-now.json'
+
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+
+# print(result)
+location = result['iss_position']
+lat = location['latitude']
+lon = location['longitude']
+
+print('Latitude:', lat)
+print('Longitude:', lon)
+
+screen = turtle.Screen()
+screen.setup(1280, 640)
+screen.bgpic('map.gif')
+screen.setworldcoordinates(-180, -90, 180 ,90)
+
+screen.register_shape('iss.gif')
+iss = turtle.Turtle()
+iss.shape('iss.gif')
+iss.setheading(90)
+
+iss.penup()
+iss.goto(float(lon), float(lat))
+
+count = 0
+while True:
+    count += 1
+    if count % 1000 == 0:
+        try:
+            # make a new request 
+            response = urllib.request.urlopen(url)
+            result = json.loads(response.read())
+
+            location = result['iss_position']
+            lat = location['latitude']
+            lon = location['longitude']
+            print(lon, lat)
+
+            # move ISS
+            iss.goto(float(lon), float(lat))
+            iss.pendown()
+        except:
+            print('something bad happened')
 
 
-# TODO: Step 3 When will the ISS be overhead?  Note: Change from Houston to Roseville
 
 
-# TODO: Step 4 Rewrite the program in a format that uses functions
-
-
-# TODO: Step 5 How could you make the program more interactive?
-# Click for new overhead location?
-# Ask User for location for overhead location?
-# Make the program update automatically?
-
-# use screen.mainloop() to keep the window open
+screen.mainloop()
